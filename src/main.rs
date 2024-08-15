77c6e7280302b8597d83eaf2a3e5c343ee82e204
Kai D. <gkai70263@gmail.com>
2024-08-15 13:58:49 -0400

    add comments

--- src/main.rs
+++ src/main.rs
@@ -46,27 +46,53 @@ impl Lottery {
         total
     }
 
+    /// ## Next Process
+    /// 
+    /// returns a random process based on their amount of tickets.
+    /// Lottery scheduling works by using randomness to find a process, while
+    /// the randomness is influenced by their total number of tickets.
+    /// 
+    /// Higher tickets = higher chances of winning the lottery.
+    /// 
+    /// ### Pros
+    ///     - Easy to implement
+    ///     - Lottery scheduling is deterministic
+    ///     - Lottery scheduling is fair
+    /// 
+    /// ### Cons
+    ///     - Can slow up if there's a lot of processes
+    ///     - Easy to manipulate and have more processes running faster
     fn next_process(&mut self) -> Result<&Process, LotErr> {
         let mut rng = rand::thread_rng();
+
         if self.processes.is_empty() {
             return Err(LotErr::NoProcesses)
         }
 
+        // generating a random range from 0-total tickets
+        // this will be used to determine which process will win
         let winner = rng.gen_range(0..self.get_total_tickets());
-        let mut counter = 0;
 
+        // add the tickets to this number to determine the process that
+        // will win
+        let mut cumulative_tickets = 0;
+
+        // iterate over the processes
         let mut iter = self.processes.iter();
 
+        // for each process
         for process in &mut iter {
-            counter += process.tickets();
+            // we add their tickets
+            cumulative_tickets += process.tickets();
 
-            if counter > winner {
+            // if we reach the winner, we return that process
+            if cumulative_tickets > winner {
                 return Ok(process);
             }
         }
         
+        /* edge case */
         Err(LotErr::NoProcesses)
-
     }
 
     fn ended(&self) -> bool {
@@ -85,6 +111,8 @@ fn main() {
     lot.add_process(proc2);
     lot.add_process(proc3);
 
+    /* a majority of these choices will be process B. Process A will run 2nd most, and C will only run once/twice. */
+    /* reference: https://pages.cs.wisc.edu/~remzi/OSTEP/cpu-sched-lottery.pdf */
     for _ in 0..15 {
         if lot.ended() {
             break;
